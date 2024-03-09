2d7f31d2bd3b0c4bca14212e3e9b2faf0691cf6d
Esra <eisra3000ese@gmail.com>
2024-03-10 01:29:19 +0300

    update

---
+++ 0-bubble_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+
+/**
+  * bubble_sort - a function that sorts an array of integers
+  * @array: a pointer to an array to sort
+  * @size: the size
+  */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, *sort;
+
+	for (i = 0; i <  size - 1; i++)
+	{
+		for (j = 0; j < size - 2; j++)
+		{
+			if (array[i]  > array[i + 1])
+			{
+
+			}
+		}
+	}
+
+}
